116500caa8fafa1fa4de649344e65b1bf72946a3
hochausy05 <hochausyhp@gmail.com>
2024-05-16 00:34:43 +0700

    new add

---
+++ src/main/java/com/mycompany/mahoaa/MaHoaa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahoaa;
+
+/**
+ *
+ * @author LEGION
+ */
+public class MaHoaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
